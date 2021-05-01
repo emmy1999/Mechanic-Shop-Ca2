bc0b3f4be1e81cbed132356e58cb9a5cdcc77d98
emmy1999 <emmanuelaloysious@gmail.com>
2021-05-01 01:28:10 +0000

    added validation in server

--- models/carpart-m.js
+++ models/carpart-m.js
@@ -7,23 +7,32 @@ const partSchema = new mongoose.Schema({ // defining the schema of the database 
 // Defining the structure of my database collection. 
 Manufacturer: {
 type: String,// defining the type of 
-required: true // defining the data type in my collection
+required: true,
+minLength: 3,
+max: 25 // defining the data type in my collection
 
 },
 
 Model: {
 type: String,
-required: true
+required: true,
+minLength: 3,
+max: 20
 },
 
 Part: {
 type: String,
-required: true
+required: true,
+minLength: 3,
+max: 25
+
 },
 
 Price:{
 type: Number,
-required: true
+required: true,
+min: 1,
+max: 999999
 }
 
 
